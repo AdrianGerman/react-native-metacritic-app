68d73e168bb66780bdc007acb936eda5d645dad6
AdrianGerman <adrian.german1019@gmail.com>
2024-07-31 08:42:22 -0600

    load imagen and dark mode

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View, Image } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={{ uri: "https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg" }}
+        style={{ width: 215, height: 294 }}
+      />
+      <Text style={styles.text}>Mi primer app</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#242424",
+    alignItems: "center",
+    justifyContent: "center"
   },
-});
+  text: {
+    color: "#fff",
+    marginTop: 12
+  }
+})
