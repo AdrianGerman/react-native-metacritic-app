cebead3f8d0ea43e658fc02bb7c1b1320e9bff52
AdrianGerman <adrian.german1019@gmail.com>
2024-08-01 07:18:28 -0600

    ReactNativeSafeAreaContext

--- App.js
+++ App.js
@@ -1,32 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { getLatestGames } from "./lib/metacritic";
-import Constants from "expo-constants";
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Main } from "./components/Main";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    getLatestGames().then((games) => {
-      setGames(games);
-    });
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <ScrollView>
-        {games.map((game) => (
-          <View key={game.slug} style={styles.card}>
-            <Image source={{ uri: game.image }} style={styles.image} />
-            <Text style={styles.title}>{game.title}</Text>
-            <Text style={styles.score}>{game.score}</Text>
-            <Text style={styles.description}>{game.description}</Text>
-          </View>
-        ))}
-      </ScrollView>
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -36,31 +20,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#242424",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: Constants.statusBarHeight,
-    padding: 12,
-  },
-  card: {
-    marginBottom: 42,
-  },
-  image: {
-    width: 107,
-    height: 147,
-    borderRadius: 10,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginTop: 10,
-    color: "#fff",
-  },
-  description: {
-    fontSize: 16,
-    color: "#eee",
-  },
-  score: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "green",
-    marginBottom: 10,
+    paddingHorizontal: 12,
   },
 });
