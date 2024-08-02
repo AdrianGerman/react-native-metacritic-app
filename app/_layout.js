91a0aa0ec80ab33d91b013f7ecc2f26c7015e9c6
AdrianGerman <adrian.german1019@gmail.com>
2024-08-02 09:35:41 -0600

    dynamic routes and style correction

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function Layout() {
   return (
     <View className="flex-1 bg-[#242424] items-center justify-center">
       <Slot />
+      <StatusBar style="light" />
     </View>
   );
 }
