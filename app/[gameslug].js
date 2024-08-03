2d64b7f7c5dedd69b1caf8df00d2ed8edb7d1179
AdrianGerman <adrian.german1019@gmail.com>
2024-08-03 09:02:34 -0600

    change header at page level and name change

--- app/[gameslug].js
+++ app/[gameslug].js
@@ -1,16 +1,23 @@
-import { Link } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { Text, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { Screen } from "../components/Screen";
 
 export default function Detail() {
-  const { id } = useLocalSearchParams();
+  const { gameslug } = useLocalSearchParams();
   return (
     <Screen>
+      <Stack.Screen
+        options={{
+          headerLeft: () => {},
+          headerRight: () => {},
+          headerTitle: "No title",
+        }}
+      />
       <View className="flex-1 justify-center items-center">
         <Text className="text-white font-bold text-2xl">Detalle del juego</Text>
         <Text className="text-white font-bold mb-8 text-2xl text-center">
-          {id}
+          {gameslug}
         </Text>
         <Link href="/" className="text-blue-500">
           Volver
